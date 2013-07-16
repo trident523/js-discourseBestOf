c5006f02d02bf2d5adb2254bf6f038cbdd663a57
bent <bent@Bonk.(none)>
2013-07-17 06:37:59 +1000

    Initial commit with actual Jekyll intergration. Docs soon!

--- javascripts/js-discourse.js
+++ javascripts/js-discourse.js
@@ -3,7 +3,7 @@ var COMMENTS = 4;
 //Update time, default is 5 min. This jumps the user's position a little on the page, so don't set it super low.
 var UPDATE_TIME = 300000;
 //URL to your forums. Exclude traling slash! ex: http://meta.discourse.org
-var BASE_URL = "http://meta.discourse.org";
+var BASE_URL = "http://forums.cityfellas.com";
 
 //Topic collision avoidance. Since in this version, discourse posts are not created, make sure you create a post with the same title as your post!
 //In the future, it might be worth comparing the topic creator ID with one hard coded here.
@@ -15,16 +15,8 @@ var scroll = $(window).scrollTop();
 var grv;
 $(document).ready(function go() {
 $('#comments').html('Loading...');
-    $.getJSON(BASE_URL + '/search.json?term=' + SLUG.replace(/\-/g, " "), function (b) {
-	try{
-	$('#comments').html("No responses yet.. Go start the discussion at " + "<a id=\"boardurl\">" + "the forums." + "</a>"); //JQuery throws a cross-domain error if the response is not json; i.e no responses yet. This text is removed in the next method.
-        $('#boardurl').attr('href', BASE_URL + b[0].results[0].url);
-	} catch (err){
-	$('#comments').html("No thread yet.. Go start the discussion at " + "<a id=\"boardurl\">" + "the forums." + "</a>"); //JQuery throws a cross-domain error if the response is not json; i.e no responses yet. This text is removed in the next method.
-        $('#boardurl').attr('href', BASE_URL);	
-	}
         try {
-            $.getJSON(BASE_URL + b[0].results[0].url + '/wordpress.json?best=' + COMMENTS, function (a) {
+            $.getJSON(BASE_URL + '/t/' + $('.comments').attr('tid') + '/wordpress.json?best=' + COMMENTS, function (a) {
                 if (a.posts) {
                     $('#comments').html('');
 
@@ -46,7 +38,6 @@ $('#comments').html('Loading...');
             $('#boardurl').attr('href', BASE_URL);
 
         }
-    });
     $("html").scrollTop(scroll);
     setTimeout(go, UPDATE_TIME);
 });
