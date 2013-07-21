6dd76990457b16a8492122bb252523012c8b6b49
Ben T <bentucci523@gmail.com>
2013-07-21 14:36:13 -0400

    Update gen.rb

--- _plugins/gen.rb
+++ _plugins/gen.rb
@@ -5,42 +5,57 @@ require 'pstore'
 module Jekyll
   class DiscourseComments < Liquid::Tag
 
-    def render(context)
-	if (Jekyll.configuration({})['discourse_api_key'].nil?)
-		raise "API key not set in config! Please set discourse_api_key"
-	end
+	def render(context)
+        	if (Jekyll.configuration({})['discourse_api_key'].nil?)
+                	raise "API key not set in config! Please set discourse_api_key"
+	        end
+        	stor = PStore.new("_discourse/" + context.environments.first["page"]["title"].gsub('/','slash') + ".pstore")
+        	posted = false
+        	@sleep = 0
+        	if(!File.exist?("_discourse/" + context.environments.first["page"]["title"].gsub('/','slash') + ".pstore"))
+        	until posted
+        		sleep @sleep
+                	puts "We're making a new post for title:" + context.environments.first["page"]["title"].gsub('/','slash')
+                	@result = HTTParty.post(Jekyll.configuration({})['discourse_api_url'] + "/posts",
+                	:body => { :api_key => Jekyll.configuration({})['discourse_api_key'],
+                           :api_username => Jekyll.configuration({})['discourse_api_username'],
+                           :title => context.environments.first["page"]["title"],
+                           :raw => "From the blog: " + context.environments.first["page"]["title"] + "\n" + Jekyll.configuration({})['url'] + '/' + context.environments.first["page"]["url"] + ")",
+                           :category => Jekyll.configuration({})['discourse_api_category'],
+                           :skip_validations => 'true'
+                         }.to_json,
+                            :headers => { 'Content-Type' => 'application/json' } );
+                	if(!@result['errors'])
+                        	parsing = JSON.parse(@result.body);
 
-	stor = PStore.new("_discourse/" + context.environments.first["page"]["title"].gsub('/','slash') + ".pstore")	
-
-	if(!File.exist?("_discourse/" + context.environments.first["page"]["title"].gsub('/','slash') + ".pstore"))
-		puts "We're making a new post for title:" + context.environments.first["page"]["title"].gsub('/','slash')
-		@result = HTTParty.post(Jekyll.configuration({})['discourse_api_url'] + "/posts", 
-	        :body => { :api_key => Jekyll.configuration({})['discourse_api_key'], 
-            		   :api_username => Jekyll.configuration({})['discourse_api_username'], 
-	   		   :title => context.environments.first["page"]["title"], 
-			   :raw => "From the blog: " + context.environments.first["page"]["title"] + "\n" + Jekyll.configuration({})['url'] + '/' + context.environments.first["page"]["url"] + ")", 
-			   :category => Jekyll.configuration({})['discourse_api_category'],
-			   :skip_validations => 'true'
-             		 }.to_json,
-			    :headers => { 'Content-Type' => 'application/json' } );
-	
-			parsing = JSON.parse(@result.body);
-
-			stor.transaction do
-				stor['topic_id'] = parsing['topic_id']
-			end
-
-		parsing['id']
-	end
-	
-	else
-		found = -1
-		stor.transaction do
-			found = stor['topic_id']
-		end
-		"tid = \"#{found}\""
-	end
+                        	stor.transaction do
+                                	stor['topic_id'] = parsing['topic_id']
+                        	end
+                        	posted = true
+                        else
+                        	puts "\033[31m" + "We got these errors from discourse: " + "\033[0m"
+                        	puts "\033[35m"
+                        	puts @result['errors']
+                        	puts "\033[0m"
+                        if(@result.code == 429)
+                                puts "We were asked to slow down! Re-trying in: " + @sleep + " seconds."
+                                @sleep += 5
+                        else
+                                raise "Other API error"
+                        end
+                end
+        parsing['id']
+        end
+        else
+                found = -1
+                stor.transaction do
+                        found = stor['topic_id']
+                end
+                posted = true
+                "tid = \"#{found}\""
+        end
     end
+  end
 end
 
 Liquid::Template.register_tag('discourse_comments', Jekyll::DiscourseComments)
