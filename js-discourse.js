cb2253524f80abed60c6c345f635f3cdbfa3f56e
Ben T <bentucci523@gmail.com>
2013-08-01 17:01:59 -0400

    Added additional config flags
    This allows for posts with low comments to still print comments.

--- js-discourse.js
+++ js-discourse.js
@@ -3,7 +3,13 @@ var COMMENTS = 4;
 //Update time, default is 5 min. This jumps the user's position a little on the page, so don't set it super low.
 var UPDATE_TIME = 300000;
 //URL to your forums. Exclude traling slash! ex: http://meta.discourse.org
-var BASE_URL = "http://forums.cityfellas.com";
+var BASE_URL = "http://meta.discourse.org";
+//Minimum user trust level to allow comments from. Default of all users. DANGER: new user posts can show up, on a high traffic site this can be bad!
+var min_trust_level = 0; 
+//Minimum score to allow. DANGER: This can allow spam comments to slip through without being read! 
+var min_score = 0;
+//Minimum number of replies before displaying comments. We always strive to display the number above.
+var min_replies = 1;
 
 //In the future, it might be worth comparing the topic creator ID with one hard coded here.
 //Stuff NOT to change!
@@ -14,7 +20,7 @@ var grv;
 jQuery(document).ready(function go() {
 jQuery('#comments').html('Loading...');
         try {
-            jQuery.getJSON(BASE_URL + '/t/' + jQuery('#comments').attr('tid') + '/wordpress.json?best=' + COMMENTS, function (a) {
+            jQuery.getJSON(BASE_URL + '/t/' + jQuery('#comments').attr('tid') + '/wordpress.json?best=' + COMMENTS + '&min_trust_level=' + min_trust_level + '&min_score=' + min_score + '&min_replies=' + min_replies, function (a) {
                 if (a.posts) {
                     jQuery('#comments').html('');
 
