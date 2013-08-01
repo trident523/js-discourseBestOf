934b683a5566b713fb957417fa225d296b862f7d
Ben T <bentucci523@gmail.com>
2013-07-31 22:56:07 -0400

    Specifying jQuery for now

--- js-discourse.js
+++ js-discourse.js
@@ -7,37 +7,37 @@ var BASE_URL = "http://forums.cityfellas.com";
 
 //In the future, it might be worth comparing the topic creator ID with one hard coded here.
 //Stuff NOT to change!
-var TITLE = $(document).attr('title').toLowerCase();
+var TITLE = jQuery(document).attr('title').toLowerCase();
 var SLUG = TITLE.substring(0, TITLE.lastIndexOf('-'));
-var scroll = $(window).scrollTop();
+var scroll = jQuery(window).scrollTop();
 var grv;
-$(document).ready(function go() {
-$('#comments').html('Loading...');
+jQuery(document).ready(function go() {
+jQuery('#comments').html('Loading...');
         try {
-            $.getJSON(BASE_URL + '/t/' + $('#comments').attr('tid') + '/wordpress.json?best=' + COMMENTS, function (a) {
+            jQuery.getJSON(BASE_URL + '/t/' + jQuery('#comments').attr('tid') + '/wordpress.json?best=' + COMMENTS, function (a) {
                 if (a.posts) {
-                    $('#comments').html('');
+                    jQuery('#comments').html('');
 
-                    $.each(a.posts, function (i, v) {
-                        $('#comments').append("<div id=\"toset\">" + "<img width=\"45\" id=\"grav" + i + "\">" + "<b>" + a.posts[i].name + "</b>" + "  said  " + a.posts[i].cooked + "</div>" + "<hr>");
+                    jQuery.each(a.posts, function (i, v) {
+                        jQuery('#comments').append("<div id=\"toset\">" + "<img width=\"45\" id=\"grav" + i + "\">" + "<b>" + a.posts[i].name + "</b>" + "  said  " + a.posts[i].cooked + "</div>" + "<hr>");
                         grv = a.posts[i].avatar_template.replace('\{size\}', "45");
-                        $('#grav' + i).attr('src', grv);
-                        $('#toset').attr('id', 'post' + i);
+                        jQuery('#grav' + i).attr('src', grv);
+                        jQuery('#toset').attr('id', 'post' + i);
                     });
-                    $('#comments').append("<p> Keep chatting about this post at <a id=\"togo\"> the forums.</a> There are " + (a.filtered_posts_count - a.posts.length - 1) + " other replies.");
-                    $('#togo').attr('href', BASE_URL + '/t/load/' + $('#comments').attr('tid'));
+                    jQuery('#comments').append("<p> Keep chatting about this post at <a id=\"togo\"> the forums.</a> There are " + (a.filtered_posts_count - a.posts.length - 1) + " other replies.");
+                    jQuery('#togo').attr('href', BASE_URL + '/t/load/' + jQuery('#comments').attr('tid'));
                 } else {
                 
-                    $('#comments').html("No responses yet. Go start the discussion at " + "<a id=\"boardurl\">" + "the forums." + "</a>");
-                    $('#boardurl').attr('href', BASE_URL);
+                    jQuery('#comments').html("No responses yet. Go start the discussion at " + "<a id=\"boardurl\">" + "the forums." + "</a>");
+                    jQuery('#boardurl').attr('href', BASE_URL);
                 }
             });
         } catch (err) {
                 console.log(err);
-            $('#comments').html("Nothing close to this page title. Go start the discussion at " + "<a id=\"boardurl\">" + "the forums." + "</a>");
-            $('#boardurl').attr('href', BASE_URL);
+            jQuery('#comments').html("Nothing close to this page title. Go start the discussion at " + "<a id=\"boardurl\">" + "the forums." + "</a>");
+            jQuery('#boardurl').attr('href', BASE_URL);
         }
-    $("html").scrollTop(scroll);
+    jQuery("html").scrollTop(scroll);
     setTimeout(go, UPDATE_TIME);
 });
 
